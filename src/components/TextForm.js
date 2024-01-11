69086725ea01309a48e488e135ebe8b9acbf3e35
Mayur Wardhani <mayurwardhani@gmail.com>
2024-01-11 18:00:19 +0530

    fix issues

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,6 +21,20 @@ export default function TextForm(props) {
         
     }
 
+    const handleCopy = () => {
+        var text= document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
+        props.showAlert("Copied to Clipboard", "Success");
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra Spaces removed", "Success");
+    }
+
     const clearText = () => {
         setText("")
         props.showAlert("Text Cleared", "Success")
@@ -35,16 +49,18 @@ export default function TextForm(props) {
     <textarea className="form-control" value = {text} onChange={handleOnChange} id="myBox" rows="7"></textarea>
     </div>
 
-    <button className='btn btn-outline-primary brn-primary mx-2'  onClick={handleUpClick}>Convert to Uppercase</button>
-    <button className='btn btn-outline-primary brn-primary mx-2' onClick={handleLowClick}>Convert to Lowercase</button>
-    <button className='btn btn-outline-primary brn-primary mx-2' onClick={clearText}>Clear Text</button>
+    <button disabled = {text.length===0} className='btn btn-outline-primary brn-primary mx-2 my-1'  onClick={handleUpClick}>Convert to Uppercase</button>
+    <button disabled = {text.length===0} className='btn btn-outline-primary brn-primary mx-2 my-1' onClick={handleLowClick}>Convert to Lowercase</button>
+    <button disabled = {text.length===0} className='btn btn-outline-primary brn-primary mx-2 my-1' onClick={clearText}>Clear Text</button>
+    <button disabled = {text.length===0} className='btn btn-outline-primary brn-primary mx-2 my-1' onClick={handleCopy}>Copy Text</button>
+    <button disabled = {text.length===0} className='btn btn-outline-primary brn-primary mx-2 my-1' onClick={handleExtraSpaces}>Remove Extra Spaces</button>
    </div>
    <div className="container my-3">
     <h2> Text Summary</h2>
-    <p>{text.split(" ").length} words and {text.length} characters</p>
-    <p>{0.008 * text.split(" ").length} mintues reading time</p>
+    <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+    <p>{0.008 * text.split.length} mintues reading time</p>
     <h3>Preview</h3>
-    <p>{text}</p>
+    <p>{text.length>0?text:"Nothing to preview"}</p>
    </div>
    </>
   )
